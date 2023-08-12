5e119660284b19347cda17fdcffa38d832b670b1
william hill <williamhill0831@Hotmail.com>
2023-08-12 14:26:27 -0400

    added single linked list

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+// class Node {
+//   constructor(data, next = null) {
+//     this.data = data;
+//     this.next = next;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null; // null by default
+//     this.size = 0;
+//   }
+// }
+
 class Node {
-  constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null; // null by default
-    this.size = 0;
-  }
-}
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+const print = (head) => {
+  if (head === null) return;
+  console.log(head.val);
+  print(head.next);
+};
+
+// const print = (head) => {
+//   let current = head;
+//   while (current !== null) {
+//     console.log(current.val);
+//     current = current.next;
+//   }
+// };
+
+print(a);
